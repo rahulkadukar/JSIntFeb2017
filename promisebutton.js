8da10c72630ab52c2873a35f77eff4808beffcb5
Simon Roberts <simon@dancingcloudservices.com>
2017-03-01 17:02:46 -0500

    Added promise example

--- promisebutton.js
+++ promisebutton.js
@@ -1,19 +1,18 @@
 "use strict";
 (function () {
     console.log('startup...');
-
     function buttonPromise(btn) {
-        return new Promise(function(resolve, reject){
-            btn.addEventListener('click', function(e) {
+        return new Promise(function (resolve, reject) {
+            btn.addEventListener('click', function (e) {
                 resolve("click");
             });
         });
     }
-    
+
     function pausePromise(data) {
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             console.log('pause starting, with ' + data);
-            setTimeout(function(){
+            setTimeout(function () {
                 if (Math.random() < 0.8) {
                     console.log('intermediate completing...');
                     resolve("Intermediate processor handled " + data);
@@ -23,14 +22,20 @@
             }, 2000);
         });
     }
-    
-    let getChatty = s=>"chatty compute job gave " + s;
-    
+
+    let getChatty = s => "chatty compute job gave " + s;
     let button = document.getElementById("mybutton");
     buttonPromise(button)
-            .then(getChatty)
+            .then(getChatty).then(undefined, s => "I'm recovering from " + s)
             .then(pausePromise)
+            .then(undefined, s => {
+                if (Math.random() > 0.8) {
+                    return "I'm recovering from " + s;
+                } else {
+                    throw new Error("double failure");
+                }
+            })
             .then(getChatty)
-            .then(s=>console.log(s));
+            .then(s => console.log(s), s => console.log("it broke!!!! " + s));
     console.log("xxxxxx");
 }());
